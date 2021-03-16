6d9491a60934ef187cd1a0bfd7312ae20522d92b
Jesse Gonzalez <jgonzalea@life360.com>
2021-03-16 16:20:55 -0500

    Adding "load_balancers" and "target_group_arns" to ignore lifecycle ruls per TF documentation when attaching ALBs

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+### v2.9.2-fork - ignore_lifelcycle rules for ASG ALB attachment
+
+- Documentation for the [aws_autoscaling_attachment](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_attachment) resource states that "load_balancers" and "target_group_arns" should be ignored and included in the lifecycle configuration block.
+
+### v2.9.1-fork - ignore_lifecycle rule for desired_capacity
+
+-  https://life360.slack.com/archives/G03C5TNUN/p1592332987300000
--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_autoscaling_group" "this" {
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes        = ["desired_capacity"]
+    ignore_changes        = ["desired_capacity", "load_balancers", "target_group_arns"]
   }
 }
 
@@ -115,7 +115,7 @@ resource "aws_autoscaling_group" "this_with_initial_lifecycle_hook" {
 
   lifecycle {
     create_before_destroy = true
-    ignore_changes        = ["desired_capacity"]
+    ignore_changes        = ["desired_capacity", "load_balancers", "target_group_arns"]
   }
 }
 
